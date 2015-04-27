a7b6d3bd543c36d5e52c6c7fb1b5569edfbcc408
l <l@jusss.org>
2015-04-27 22:15:30 +0000

    function program

--- python/lisp2html.py
+++ python/lisp2html.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python3
 
-### "(html (body ...))" -> " ( html  ( body ... ) ) " -> [ '(', 'html', ... ' ) ', ')'] -> [ '<','html',...'>'] -> "<html> ...</html>"
+### "(html ...)" -> [ '(', 'html', ... ')'] -> ['</html>',...'<html>'] -> ['<html>'...'</html>'] -> "<html>...</html>"
 
 ### set a list as a stack to store one token after '<', convert_list to store the list after changingn
 global stack=[]
-global convert_list=[]
+global clist=[]
 
-### define a function to push and pop the stack
-def push(token):
-    stack.append(token)
-    
-def pop():
-    first=stack[0]
-    stack.remove_first()
-    return first
+def car(alist):
+    return alist[0]
+
+def cdr(alist):
+    return alist[1:]
+
+def cons(atom,alist):
+    return [atom]+alist
+
+def reverse(alist):
+    return alist[-1:]
 
 def tokenize(s):
     "Convert a string into a list of tokens."
     return s.replace('(',' ( ').replace(')',' ) ').split()
 
-def convert(alist):
-
-    if stack[0]='(':
-        convert_list.appen('<')
-        push(stack[1])
-        stack.remove_first()
-        return convert(alist)
+def convert(alist, clist, stack):
+    if not alist:
+        return clist
     else:
-        if stack[0]=')':
-            stack.append(pop())
-            stack.append('>')
-            stack.remove_first()
-            return convert(alist)
+        if car(alist) == '(':
+            return convert(cdr(alist), cons('<'+car(cdr(alist))+'>',clist), cons(car(cdr(alist)),stack))
         else:
-            stack.append(stack[0])
-            stack.remove_first()
-            return convert(alist)
+            if car(alist) == ')':
+                return convert(cdr(alist), cons('</'+car(stack)+'>',clist), cdr(stack))
+            else:
+                return convert(cdr(alist), cons(car(alist),clist), stack)
 
-def parse(string):
-    return list2string(convert(tokenize(string)))
-
-### list.remove_first() shoule turn  ['1', '2'...] to ['2'...] like (car ...) in lisp
+print(list2string(reverse(convert(tokenize(s), clist, stack))))
 
 
 
