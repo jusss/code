eae2e392507d5ff3aaf1a649b8977d0335ec2e8c
l <l@jusss.org>
2015-12-04 22:07:54 +0000

    set timeout for recv_server

--- python/part-v-dns-relay-client.py
+++ python/part-v-dns-relay-client.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 import os, sys, socket, threading
 
@@ -6,65 +5,94 @@ local_addr = ('127.0.0.1',53)
 # server_addr should be your vps'ip and port
 server_addr = ('1.1.1.1',66666)
 recv_send_size = 102400
-
 query_addr_ID = []
-
-local_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-local_socket.bind(local_addr)
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect vps
-try:
-    server_socket.connect(server_addr)
-except Exception as e:
-    print(e)
-    sys.exit()
-
-print('it already has connected to dns relay server.')
+switch = 1
+switch_on = 1
+switch_off = 0
     
 def recv_local():
+    global switch, switch_on, switch_off
     while True:
-        query_data, query_addr = local_socket.recvfrom(recv_send_size)
-        print('receive from: ', query_addr)
-        print(query_data)
-        query_addr_ID.append(query_addr)
-        query_addr_ID.append(query_data[0:2])
-        # reverse data for keeping away from poison
-        server_socket.send(query_data[::-1])
+        if switch == switch_off:
+            print('recv_local thread got switch_off')
+            break
+        try:
+            query_data, query_addr = local_socket.recvfrom(recv_send_size)
+            print('receive from: ', query_addr)
+            print(query_data)
+            query_addr_ID.append(query_addr)
+            query_addr_ID.append(query_data[0:2])
+            # reverse data for keeping away from poison
+            try:
+                server_socket.send(query_data[::-1])
+            except Exception as e:
+                print(e)
+                break
+        except socket.timeout as e:
+            pass
+    switch = switch_off
 
 def recv_server():
+    global switch, switch_on, switch_off
     while True:
-        answer_data = server_socket.recv(recv_send_size)
-        if not answer_data:
-            print('disconnect...')
-            sys.exit()
-        else:
+        if switch == switch_off:
+            print('recv_server thread got switch_off')
+            break
+        try:
+            answer_data = server_socket.recv(recv_send_size)
+            if not answer_data:
+                print('recv empty strings from server')
+                break
             print('receive from: ', server_addr)
             print(answer_data)
-        
-        if query_addr_ID:
-            match_ID_index = query_addr_ID.index(answer_data[0:2])
-            match_query_addr = query_addr_ID[match_ID_index - 1]
-            
-            if match_query_addr :
-                local_socket.sendto(answer_data, match_query_addr)
-                del query_addr_ID[match_ID_index]
-                del query_addr_ID[match_ID_index - 1]
+            if query_addr_ID:
+                match_ID_index = query_addr_ID.index(answer_data[0:2])
+                match_query_addr = query_addr_ID[match_ID_index - 1]
+                if match_query_addr :
+                    try:
+                        local_socket.sendto(answer_data, match_query_addr)
+                        del query_addr_ID[match_ID_index]
+                        del query_addr_ID[match_ID_index - 1]
+                        #query_addr_ID.remove(answer_data[0:2])
+                        #query_addr_ID.remove(match_query_addr)
+                        #ok, query_addr_ID.remove() will remove something make sendto() error like match_query_addr is not a tuple, so just use del to delete element.
+                    except Exception as e:
+                        print(e)
+                        break
+        except socket.timeout as e:
+            print('recv nothing over 5 minutes, timeout')
+            break
+    switch = switch_off
                 
 # here wo go, begin
-try:
-    thread_recv_local = threading.Thread(target=recv_local)
-    thread_recv_server = threading.Thread(target=recv_server)
-    thread_recv_local.start()
-    thread_recv_server.start()
-    thread_recv_local.join()
-    thread_recv_server.join()
-except Exception as e:
-    print(e)
+while True:
+    switch = switch_on
+    local_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+    local_socket.bind(local_addr)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # set timeout to unblock thread
+    local_socket.settimeout(2)
+    server_socket.settimeout(300)
+    # connect vps
+    try:
+        server_socket.connect(server_addr)
+    except Exception as e:
+        print(e)
+        sys.exit()
+    print('it already has connected to dns relay server.')
+    try:
+        thread_recv_local = threading.Thread(target=recv_local)
+        thread_recv_server = threading.Thread(target=recv_server)
+        thread_recv_local.start()
+        thread_recv_server.start()
+        thread_recv_local.join()
+        thread_recv_server.join()
+    except Exception as e:
+        print(e)
     local_socket.close()
     server_socket.close()
 
+    
 """
 使用list保存query_addr和query_data的前 2 Bytes，这2 Bytes 是dns数据包的随机标志ID, dns server回复的answer_data的前2 Bytes必须和它一样
 这样使用匹配ID在list中寻找到对应的query_addr，可以使用双线程而不怕发错query_addr
