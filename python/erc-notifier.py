c1618260bff8bb950ffd91ab9b6991b82432d2fa
l <l@jusss.org>
2016-12-12 18:50:11 +0000

    erc notifier, but do not work

--- python/erc-notifier.py
+++ python/erc-notifier.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import sys, os, subprocess, signal
-
-### usage: $~/lab/erc-notifier.py title message
+#usage $~/lab/erc-notifier.py title message
 ### pop up the first notify window, and kill it when pop up the second notify window
 controler=0
 previous_pid=0
 if controler == 0:
     p0=subprocess.Popen(['/home/jusss/lab/notifier.py', sys.argv[1], sys.argv[2]], shell=False)
-    os.system("sed -i -e 's/controler=0/controler=1/' ~/lab/erc-notifier.py")
-    os.system("sed -i -e 's/previous_pid=0/previous_pid=" + str(p0.pid) + "/' ~/lab/erc-notifier.py")
+    ### just replace once! sed -i '0,/a/ s//e/' example
+    os.system("sed -i -e '0,/controler=0/ s//controler=1/' ~/lab/erc-notifier.py")
+    os.system("sed -i -e '0,/previous_pid=0/ s//previous_pid=" + str(p0.pid) + "/' ~/lab/erc-notifier.py")
 if controler == 1:
     try:
         ### consider if you click ok, and then process exit, so the process won't exist anymore
@@ -16,4 +16,4 @@ if controler == 1:
     except Exception as e:
         print(e)
     p=subprocess.Popen(['/home/jusss/lab/notifier.py', sys.argv[1], sys.argv[2]], shell=False)
-    os.system("sed -i -e 's/previous_pid=" + str(previous_pid) + "/previous_pid=" + str(p.pid) + "/' ~/lab/erc-notifier.py")
+    os.system("sed -i -e '0,/previous_pid=" + str(previous_pid) + "/ s//previous_pid=" + str(p.pid) + "/' ~/lab/erc-notifier.py")
