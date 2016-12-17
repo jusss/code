c17ca03eca106a147b5855be5f424ae0c47a8186
l <l@jusss.org>
2016-12-17 22:31:20 +0000

    check for new mail

---
+++ python/check-for-new-mail.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os, subprocess, time, sys
+### use offlineimap download mail, and store as maildir type not mbox
+### check $MAIL/INBOX/new/, if it's empty directory then it means no new mail
+### */30 * * * * ~/lab/check-for-new-mail.py    write this into crontab with crontab -e
+mail_stored_path = "/home/jusss/Mail/qq/INBOX/new/"
+notifier_path = "/home/jusss/lab/notifier.py"
+a_list = os.listdir(mail_stored_path)
+if a_list == []:
+    sys.exit()
+else:
+    subprocess.Popen([notifier_path, "Mail", "New Mail"], shell=False)
+    ### prevent this script is too fast to end that make Popen have not done yet
+    time.sleep(3)
+
+    
+
+
