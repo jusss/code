8557b89025714a7effa794cb0d7298a03974ab6e
l <l@jusss.org>
2016-12-18 09:45:15 +0000

    env DISPLAY=:0.0 check-for-new-mail.py

--- python/check-for-new-mail.py
+++ python/check-for-new-mail.py
@@ -4,13 +4,16 @@ import os, subprocess, time, sys
 ### check $MAIL/INBOX/new/, if it's empty directory then it means no new mail
 ### */30 * * * * ~/lab/check-for-new-mail.py    write this into crontab with crontab -e
 ### another way to check new mail, is thought imaple idle or compare uid or recent message
+### if you don't want set DISPLAY=0.0 here, then put it on env DISPLAY=:0.0 this-script, but
+### I don't know notifier can find the environment variables from its caller's envrionment variabels
 mail_stored_path = "/home/jusss/Mail/qq/INBOX/new/"
 notifier_path = "/home/jusss/lab/notifier.py"
+display = "DISPLAY=:0.0"
 a_list = os.listdir(mail_stored_path)
 if a_list == []:
     sys.exit()
 else:
-    subprocess.Popen([notifier_path, "Mail", "New Mail"], shell=False)
+    subprocess.Popen(["/usr/bin/env", display, notifier_path, "Mail", "New Mail"], shell=False)
     ### prevent this script is too fast to end that make Popen have not done yet
     time.sleep(3)
 
