b7c22ad2fde1c91f08f6efa1cdebd97e95caca8a
l <l@jusss.org>
2016-12-18 08:54:17 +0000

    check for new mail

--- python/check-for-new-mail.py
+++ python/check-for-new-mail.py
@@ -3,6 +3,7 @@ import os, subprocess, time, sys
 ### use offlineimap download mail, and store as maildir type not mbox
 ### check $MAIL/INBOX/new/, if it's empty directory then it means no new mail
 ### */30 * * * * ~/lab/check-for-new-mail.py    write this into crontab with crontab -e
+### another way to check new mail, is thought imaple idle or compare uid or recent message
 mail_stored_path = "/home/jusss/Mail/qq/INBOX/new/"
 notifier_path = "/home/jusss/lab/notifier.py"
 a_list = os.listdir(mail_stored_path)
