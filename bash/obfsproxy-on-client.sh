6af155dec1f0d8d5d0eb23958306421e5305d0c2
John <ifvwm1@gmail.com>
2017-11-20 22:20:03 +0800

    obfsproxy for ssh

---
+++ bash/obfsproxy-on-client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+obfsproxy scramblesuit --dest your-server-ip:port --password IWILLNEVERNEVERNEVERCOMPROMISELL client 127.0.0.1:1990
---
+++ bash/obfsproxy-on-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+obfsproxy --data-dir ~/.obfs scramblesuit --dest 127.0.0.1:1990 --password IWILLNEVERNEVERNEVERCOMPROMISELL server 0.0.0.0:19900
