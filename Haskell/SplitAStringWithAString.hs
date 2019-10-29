7407d8a97b96c2c2c6295b0dcbcae684489eb54e
John <ifvwm1@gmail.com>
2019-10-29 23:09:44 +0800

    .

--- Haskell/SplitAStringWithAString.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import SplitAlistWithAlist
-import Data.ByteString
-
-o = unpack ":nick!~... PRIVMSG #channel : "
-a = unpack " PRIVMSG #"
-
-main = print $ fmap (\i -> pack i) $ splitListWithList o a
