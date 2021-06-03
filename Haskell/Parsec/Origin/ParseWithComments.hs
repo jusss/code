7576c214ed73b2b9e9cd71b7f516c06e85c7f5c4
John <ifvwm1@gmail.com>
2021-06-04 00:38:48 +0800

    parsewithcomments.hs I2T18.hs groupPair.kt

--- Haskell/Parsec/Origin/ParseWithComments.hs
+++ Haskell/Parsec/Origin/ParseWithComments.hs
@@ -7,7 +7,7 @@ import Data.Maybe
 import qualified Control.Applicative as C
 
 parseComment :: Parser (Maybe (String,String))
-parseComment = do
+parseComment = try $ do
     spaces
     string "--"
     c <- manyTill anyChar newline
@@ -15,7 +15,7 @@ parseComment = do
     return Nothing
 
 parseAssign :: Parser (Maybe (String,String))
-parseAssign = do
+parseAssign = try $ do
   spaces
   k <- many letter
   spaces
@@ -23,19 +23,39 @@ parseAssign = do
   spaces
   v <- many (noneOf "\n ")
   spaces
-  return (Just (k,v))
+  return $ Just (k,v)
+
+parseInvalid :: Parser (Maybe (String, String))
+parseInvalid = try $ do
+    spaces
+    manyTill anyChar newline
+    spaces
+    return Nothing
+
+findIn :: String -> Either ParseError (Map String String) -> Maybe String
+findIn key (Right m) = fromRight Nothing $ fmap (!? key) (Right m)
+findIn key (Left m) = Nothing
 
 main = do
   context <- readFile "a.conf"
-  let r2 = parse (many (try parseComment <|> parseAssign)) "" context
+  let r2 = parse (many (parseComment <|> parseAssign <|> parseInvalid)) "" context
   print r2
   let r3 = fmap (filter (/= Nothing)) r2
-  if (Right [] == r3) then print "no assign, just comments"
+  if (Right [] == r3) then print "no assignment, just comments and invalid styles"
   else do
     print r3 -- Right [Just ("a","b")]
     let r9 = fmap fromList (fmap (fmap fromJust) r3)
     print r9 -- Right fromList [("a","b")]
-    print (fmap (findWithDefault " server not found" "server") r9) -- Right "abc.net"
+
+    let result = fromRight Nothing (fmap (!? "server") r9)
+    if (result == Nothing) then print "server not found"
+    else print $ fromJust result
+
+    -- another version, same as above
+    let r5 = findIn "server" r9
+    if (r5 == Nothing) then print "server not found" else print $ fromJust r5
+
+
   
 -------------
 -- a.conf
@@ -44,4 +64,3 @@ main = do
 -- b = 3
 -- server = abc.net
 -- port = 7000
-
