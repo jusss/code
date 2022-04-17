d297d293cc277f60beadaf1fa22721ab669e8062
John <ifvwm1@gmail.com>
2022-04-17 17:54:31 +0800

    .

--- python/fib.py
+++ python/fib.py
@@ -1,11 +1,13 @@
 def fib(n):
-    c = []
+    #c = []
+    c = 0
     x = 0
     y = 1
 
     # loop times by its length
     for i in range(n+1):
-        c.append(x)
+        #c.append(x)
+        c = x
         x,y = y, x+y
     return c
 
