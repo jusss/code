11b5df2ef8f25ae19341af1853e16eeb7ad70828
John <ifvwm1@gmail.com>
2023-10-19 23:07:53 +0800

    fib trampoline decorator

--- python/fib-trampoline.py
+++ python/fib-trampoline.py
@@ -1,3 +1,4 @@
+from functools import *
 def trampoline(f):
     while callable(f):
         f=f()
@@ -16,4 +17,24 @@ def fib(x,y,n):
     return lambda: fib(y,x+y,n-1)
 
 trampoline(fib(0,1,2200))
+
+def trampoline_decorator(f):
+    def wrapper(*args, **kwargs):
+         r= f(*args, **kwargs)
+         print(*args, **kwargs)
+         while iter(r):
+             r=next(r)
+         return r
+    return wrapper
+
+@trampoline_decorator
+def fib_t(x,y,n,cond):
+    if not cond:
+        return x
+    if n == 0:
+        return lambda: fib_t(y,x+y,n-1, False)
+    print(x)
+    yield fib_t(y,x+y,n-1, cond)
+
+fib_t(0,1,5, True)
 
