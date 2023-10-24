79e196ad41a85abe2358a0116ab12b0df8445b26
John <ifvwm1@gmail.com>
2023-10-24 13:35:18 +0800

    ExceptT a IO b, only run IO on Right way

--- Haskell/Monad-Transformer-ExceptT.hs
+++ Haskell/Monad-Transformer-ExceptT.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Control.Monad.Trans.Except
 import Control.Monad.IO.Class
 main = do
     -- ExceptT String IO Int; only run IO on Right way
-    runExceptT $ do
+    -- return Left a will short circuiting computation and will get Left a
+    r :: Either String () <- runExceptT $ do
         -- return construct IO a
         a <- ExceptT (return $ Right 3 :: IO (Either String Int))
         -- Left "a" won't run
         -- a <- ExceptT (return $ Left "a" :: IO (Either String Int))
         -- run IO inside Either, not need to wait Either's result to run IO
         liftIO $ print a
+    print r
     return ()
