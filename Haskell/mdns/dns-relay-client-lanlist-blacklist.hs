9964f3952f49bede21569b78f04642cbf9176a91
John <ifvwm1@gmail.com>
2023-12-12 12:55:43 +0800

    mdns add ipv4 cache only

--- Haskell/mdns/dns-relay-client-lanlist-blacklist.hs
+++ Haskell/mdns/dns-relay-client-lanlist-blacklist.hs
@@ -6,13 +6,14 @@ import Network.DNS.Decode (decode)
 import Network.DNS.Types
 import Control.Concurrent (forkIO)
 import Control.Concurrent.MVar
-import Data.Map.Strict
-import Data.ByteString (reverse)
-import Prelude hiding (reverse)
+import Data.Map.Strict hiding (empty, take, drop)
+import Data.ByteString (reverse, empty, take, drop)
+import Prelude hiding (reverse, take, drop)
 import Data.List (or, isInfixOf, replicate)
 import Data.ByteString.Char8 (unpack)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
+import Data.Word
 
 local_ip = (0,0,0,0)
 local_port = 53
@@ -48,6 +49,11 @@ blacklist = [
     ".1mxabnt.com.",
     "liangbingliang2.cn.",
     ".1bbpaqq.com.",
+    "detectportal.firefox.com",
+    "prod.cloudops.mozgcp.net",
+    "push.services.mozilla.com",
+    "prod.mozaws.net",
+    "firefox.settings.services.mozilla.com",
     ".shdndn2.cn"]
 
 -- lan_list for lan dns
@@ -61,11 +67,26 @@ lan_list = [
     ".zhimg.com.",
     ".163.com",
     ".51job.com",
+    ".jd.com",
+    ".jingdong.com",
+    ".jdpay.com",
+    ".jdcloud.com",
+    ".360buy.com",
+    ".360buyimg.com",
+    ".bilibili.com",
+    ".bilicd1.",
+    ".bilivideo.",
+    ".hdslb.",
+    ".cdn20.",
+    ".sina.com",
+    ".weibo.",
     ".doubanio."
     ]
 
 main = do
     searchMap <- newMVar $ fromList [(0,SockAddrInet local_port $ tupleToHostAddress local_ip)]
+    id_qnames <- newMVar $ fromList [(0,[""])]
+    qnames_response <- newMVar $ fromList [([""], empty)]
 
     -- as server, bind socket with special port then recvFrom socket to get data, client's ip and port, client port are constant in one connection time, reply to that constant port
     sock <- socket AF_INET Datagram 0
@@ -88,15 +109,32 @@ main = do
             {- IO only run Right way -}
             q <- ExceptT ((return . decode) msg :: IO (Either DNSError DNSMessage))
             let qnames = (unpack <$>) . (qname <$>) . question $ q
+            let qid = (identifier . header) q
+            qd <- liftIO $ readMVar qnames_response
             if or [isInfixOf i _y | i <- blacklist, _y <- qnames] then do
                 return $ ("blacked: " <>) <$> qnames
+            else if member qnames qd then do
+                -- change qid
+                let origin_id = take 2 msg
+                let altered_response = origin_id <> (drop 2 (qd ! qnames))
+
+                liftIO $ sendAllTo sock altered_response addr
+                
+                let _r = decode (qd ! qnames)
+                let _r1 = fmap (\a -> [(rrname x, rdata x) | x <- answer a]) $ _r
+
+                liftIO $ print _r1
+
+
+                return $ ("local: " <>) <$> qnames 
+
             else if or [isInfixOf i _y | i <- lan_list, _y <- qnames] then do
-                let qid = (identifier . header) q
                 liftIO $ (fromList [(qid, addr)] <>) <$> (takeMVar searchMap) >>= putMVar searchMap >>= \x -> sendAll sockDnsLan msg
+                liftIO $ (fromList [(qid, qnames)] <>) <$> (takeMVar id_qnames) >>= putMVar id_qnames
                 return $ ("lan: " <>) <$> qnames
             else do
-                let qid = (identifier . header) q
                 liftIO $ (fromList [(qid, addr)] <>) <$> (takeMVar searchMap) >>= putMVar searchMap >>= \x -> sendAll sockDns $ reverse msg
+                liftIO $ (fromList [(qid, qnames)] <>) <$> (takeMVar id_qnames) >>= putMVar id_qnames
                 return qnames
         
         case eitherResult of 
@@ -127,6 +165,20 @@ main = do
         if _r1 /= Right [] then do
             traverse print _r1
             traverse (\x -> fmap (! x) (readMVar searchMap) >>= sendAllTo sock msg) $ fmap (identifier . header) $ _r
+
+
+            -- filter ipv6 cache, cache ipv4 only
+            print $ fmap (\a -> [rrtype x | x <- answer a]) $ _r
+
+            -- Right [AAAA] is Right [28]
+            let rrtn = fmap (\a -> [ fromTYPE $ rrtype x | x <- answer a]) $ _r
+            print rrtn
+
+            case fmap (elem (28 :: Word16)) rrtn of
+                Right True -> fmap Right (print "filter ipv6 ")
+                _ ->
+                    traverse (\x -> fmap (! x) (readMVar id_qnames) >>= (\qnames -> (fromList [(qnames, msg)] <>) <$> (takeMVar qnames_response)) >>= putMVar qnames_response) $ fmap (identifier . header) $ _r
+
         else
             fmap Right (print "empty list from server")
 
@@ -138,5 +190,17 @@ main = do
         if _r1 /= Right [] then do
             traverse (\x -> putStr "lan server: " >> print x) _r1
             traverse (\x -> fmap (! x) (readMVar searchMap) >>= sendAllTo sock msg) $ fmap (identifier . header) $ _r
+
+            -- filter ipv6 cache, cache ipv4 only
+            print $ fmap (\a -> [rrtype x | x <- answer a]) $ _r
+
+            -- Right [AAAA] is Right [28]
+            let rrtn = fmap (\a -> [ fromTYPE $ rrtype x | x <- answer a]) $ _r
+            print rrtn
+
+            case fmap (elem (28 :: Word16)) rrtn of
+                Right True -> fmap Right (print "filter ipv6 ")
+                _ ->
+                    traverse (\x -> fmap (! x) (readMVar id_qnames) >>= (\qnames -> (fromList [(qnames, msg)] <>) <$> (takeMVar qnames_response)) >>= putMVar qnames_response) $ fmap (identifier . header) $ _r
         else
             fmap Right (print "empty list from lan server")
