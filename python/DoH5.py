0f711fd23288f2f76688ee7e92d546d35db5ab84
John <ifvwm1@gmail.com>
2025-01-01 13:19:37 +0800

    DNS over Https with threading

---
+++ python/DoH5.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+# cat /etc/resolv.conf
+# nameserver 127.0.0.1
+
+# vim /etc/hosts
+# 223.5.5.5   alidns.com
+# 1.12.12.12  doh.pub
+
+# there're tls and https two ways for dns, check https://github.com/paulmillr/encrypted-dns.git
+
+
+import os, sys, socket, requests
+import threading
+local_addr = ('127.0.0.1',53)
+
+ads = [
+        "sectigochina.com",
+        "qncud.com",
+        "dlrtz.com",
+        "0efghij.com",
+        "0ghijkl.com",
+        "taopianimage1.com",
+        ]
+
+google = [
+        "google-analytics.com",
+        "google.com",
+        "googleapis.com",
+        ]
+
+blacklist = ads
+
+
+a=b'\0x00\0x02'
+b=list(a)
+c=bytes(b)
+
+def parse_dns_package(_bytes):
+    _id = _bytes[0:2]
+    # _bytes :: bytes
+    _query = _bytes[12:]
+    # query :: list<int>
+    query = list(_query)
+    name = []
+
+    while True:
+        if query[0] == 0:
+            break
+        else:
+            length = query[0]
+            name.append(''.join(chr(i) for i in query[1:length+1]))
+            query = query[length+1:]
+
+    qname = '.'.join(name)
+
+    _anwser = query[3:]
+    # not implemented
+
+def thread_post(local_socket, session, url, headers):
+        while True:
+            query_data, query_addr = local_socket.recvfrom(10240)
+
+            x = threading.Thread(target= recv_local, args=(local_socket, session, url, headers, query_data, query_addr))
+            x.start()
+            # print("thread count: ", threading.active_count())
+
+
+
+_dict = {}
+def recv_local(local_socket, session, url, headers, query_data, query_addr):
+                
+                # _query :: bytes
+                _query = query_data[12:]
+                # query :: list<int>
+                query = list(_query)
+                name = []
+
+                while True:
+                    if query[0] == 0:
+                        break
+                    else:
+                        length = query[0]
+                        name.append(''.join(chr(i) for i in query[1:length+1]))
+                        query = query[length+1:]
+
+                qname = '.'.join(name)
+                # print(qname)
+
+                print(f"{query_addr} {qname}")
+                global _dict
+
+                _dict[query_data[0:2]] = query_addr
+
+                if (not any([i in qname for i in blacklist])) and ("." in qname):
+
+                    try:
+                        # requests.exceptions.ReadTimeout: HTTPSConnectionPool(host='1.1.1.1', port=443): Read timed out. (read timeout=None)
+                        res = session.post(url, data=query_data, headers=headers)
+                        answer_data = res.content
+        
+                        # print(f"anwser {answer_data[12:]}")
+                        
+                        _addr = _dict.get(answer_data[0:2])
+                        if _addr:
+                                local_socket.sendto(answer_data, _addr)
+                    except Exception as e:
+                        print(e)
+                        pass
+
+
+if __name__ == '__main__':
+
+    local_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+    local_socket.bind(local_addr)
+    session = requests.Session()
+    # url = "https://1.1.1.1/dns-query"
+    # url = "https://dns.alidns.com/dns-query"
+    url = "https://doh.pub/dns-query"
+    headers = {
+            'accept': 'application/dns-message',
+            'content-type': 'application/dns-message'
+            }
+    #recv_local(local_socket, session, url, headers)
+
+    thread_post(local_socket, session, url, headers)
