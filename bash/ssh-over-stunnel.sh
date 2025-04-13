72c7b2893712013c333efaeed070cd8b883a7eed
John <ifvwm1@gmail.com>
2025-04-13 22:42:08 +0800

    disable ipv6

--- bash/ssh-over-stunnel.sh
+++ bash/ssh-over-stunnel.sh
@@ -7,6 +7,6 @@ do
     sleep 1
     stunnel ~/stunnel.conf
     sleep 1
-    ssh -D 7070 -C -q -N -p PORT hask@moon -i ~/.ssh/hask
+    ssh -4 -D 7070 -C -q -N -p PORT hask@moon -i ~/.ssh/hask
     sleep 36
 done
