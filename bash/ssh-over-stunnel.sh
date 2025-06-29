7a83daac3796d9dff53e7d2be2970a6db5aac5e6
John <ifvwm1@gmail.com>
2025-06-29 23:25:01 +0800

    .

--- bash/ssh-over-stunnel.sh
+++ bash/ssh-over-stunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-while :
-do
+for ((i=0; i<10; i++)); do
+    echo $i
     date
     killall stunnel
     sleep 1
@@ -10,3 +10,14 @@ do
     ssh -4 -D 7070 -C -q -N -p PORT hask@moon -i ~/.ssh/hask
     sleep 36
 done
+
+# while :
+# do
+#     date
+#     killall stunnel
+#     sleep 1
+#     stunnel ~/stunnel.conf
+#     sleep 1
+#     ssh -4 -D 7070 -C -q -N -p PORT hask@moon -i ~/.ssh/hask
+#     sleep 36
+# done
