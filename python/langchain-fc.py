befa1d66abe503dff7ec7f47d703dd170ef201a7
John <ifvwm1@gmail.com>
2026-06-15 23:33:12 +0800

    langchain fc

--- python/langchain-fc.py
+++ python/langchain-fc.py
@@ -1,39 +1,79 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 from datetime import datetime
+import importlib.util
+from pathlib import Path
 import json
 
 API_KEY = "xxx"
 BASE_URL = "https://api.x.com/v1"
 MODEL = "model"
 
-tools = [{
-          "type": "function",
-             "function":{ 
-                "name": "get_weekday", 
-                "description": "get weekday from ISO date, YYYY-MM-DD", 
-                "parameters": { 
-                        "type": "object", 
-                        "properties": { 
-                                "query": { 
-                                        "type": "string", 
-                                        "description": "ISO date like YYYY-MM-DD" 
-                                }
-                        },
-                        "required": ["query"]
-                        }
-         }
-    }]
+API_KEY = "7"
+BASE_URL = "https://open.bigmodel.cn/api/coding/paas/v4/"
+MODEL = "glm-4.6v"
+
+# tools = [{
+          # "type": "function",
+             # "function":{ 
+                # "name": "get_weekday", 
+                # "description": "get weekday from ISO date, YYYY-MM-DD", 
+                # "parameters": { 
+                        # "type": "object", 
+                        # "properties": { 
+                                # "query": { 
+                                        # "type": "string", 
+                                        # "description": "ISO date like YYYY-MM-DD" 
+                                # }
+                        # },
+                        # "required": ["query"]
+                        # }
+         # }
+    # }]
+
     
-def get_weekday(a_dict: str):
-    try:
-        _date = a_dict["query"]
-        dt = datetime.fromisoformat(_date)
-        _d = {0:"星期一", 1:"星期二", 2:"星期三", 3:"星期四", 4:"星期五", 5:"星期六", 6:"星期日"}
-        return _d[dt.weekday()]
-    except Exception as e:
-        print(e)
-        return "格式错误，请输入YYYY-MM-DD格式"
+# def get_weekday(query: str):
+    # try:
+        # dt = datetime.fromisoformat(query)
+        # _d = {0:"星期一", 1:"星期二", 2:"星期三", 3:"星期四", 4:"星期五", 5:"星期六", 6:"星期日"}
+        # return _d[dt.weekday()]
+    # except Exception as e:
+        # print(e)
+        # return "格式错误，请输入YYYY-MM-DD格式"
+
+
+
+plugins_dir = Path.home() / 'chat_plugin'
+def load_tools(plugins_dir):
+    tools = []
+    # Iterate over all .py files in the directory
+    for plugin_path in plugins_dir.glob('*.json'):
+        with open(plugin_path, "r") as f:
+            content = f.read()
+            data = json.loads(content)
+            tools.append(data)
+    return tools
+
+tools = load_tools(plugins_dir)
+
+def load_functions(plugins_dir):
+    functions = {}
+    # Iterate over all .py files in the directory
+    for plugin_path in plugins_dir.glob('*.py'):
+        # Get the module name (without .py extension)
+        module_name = plugin_path.stem
+    
+        # Load the module
+        spec = importlib.util.spec_from_file_location(module_name, plugin_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+    
+        # Get the functions defined in the module
+        # functions = functions | {name: obj for name, obj in module.__dict__.items() if callable(obj)}
+        functions.update({name: obj for name, obj in module.__dict__.items() if callable(obj)})
+    return functions
+
+functions = load_functions(plugins_dir)
 
 
 llm = ChatOpenAI(
@@ -45,9 +85,11 @@ llm = ChatOpenAI(
 
 llm_with_tools=llm.bind_tools(tools)
 
-available_functions = {
-        "get_weekday": get_weekday,
-}
+# available_functions = {
+        # "get_weekday": get_weekday,
+# }
+
+available_functions = functions
 
 messages = []
 
@@ -63,7 +105,8 @@ def chat(msg):
         function_args = tool_call["args"]
         
         function_to_call = available_functions[function_name]
-        result = function_to_call(function_args)
+        print(f"function call {function_name}({function_args})")
+        result = function_to_call(**function_args)
 
         messages.append(ToolMessage(
             content=str(result),
