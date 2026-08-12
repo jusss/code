3de9ca2356106ad1b00dbb5e678819260d54ceb3
John <ifvwm1@gmail.com>
2026-08-12 21:58:16 +0800

    sqlalchemy db

--- python/sqlalchemy_db.py
+++ python/sqlalchemy_db.py
@@ -2,6 +2,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 import os
 from urllib.parse import quote_plus
+from contextlib import contextmanager
+from sqlalchemy.pool import NullPool
+
 
 # dev env
 host = ''
@@ -16,10 +19,40 @@ if os.getenv('ENV') != 'dev':
     user =''
     password=quote_plus('')
 
-engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}/{database_name}')
-Session = sessionmaker(bind=engine)
+# No idle connections
+# engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}/{database_name}',
+           # pool_pre_ping=True,  # Check connection before using
+           # poolclass=NullPool,  # No connection pooling, every request will start a new connection, connection overhead on each requests
+        # )
+
+engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}/{database_name}',
+           pool_pre_ping=True,  # Check connection before using
+           pool_size=10,
+           max_overflow=20,
+           pool_recycle=3600,    # Recycle connections after 1 hour
+           pool_timeout=30,  # Add timeout for getting connections
+        )
+
+Session = sessionmaker(bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False
+    )
+
+@contextmanager
+def get_session():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
 
 # in other file
-# from sqlalchemy_db import Session
-# with Session() as db:
+# from sqlalchemy_db import get_session
+# with get_session() as db:
     # db.query()
